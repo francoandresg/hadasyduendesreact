87ae8ea0d4f2fae93b653e24f6ff5164e3eda893
unknown <frandres9@gmail.com>
2025-11-28 22:50:34 -0300

    28-11-2025

--- src/api/managers/appointments.js
+++ src/api/managers/appointments.js
@@ -1,16 +1,16 @@
 import axios from 'utils/axios';
 
 export const getSelectorBoxes = async () => {
-  const response = await axios.get('/maintainers/boxes/', { params: { selector: true } });
+  const response = await axios.get('/boxes/selector');
   return response.data;
 };
 
 export const getSelectorClients = async () => {
-  const response = await axios.get('/maintainers/clients/', { params: { selector: true } });
+  const response = await axios.get('/clients/selector');
   return response.data;
 };
 
 export const getSelectorRoles = async () => {
-  const response = await axios.get('/maintainers/roles/', { params: { selector: true } });
+  const response = await axios.get('/roles/selector');
   return response.data;
 };
